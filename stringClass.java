015cf069dd2048933f639adefa4d99f3a3bd1584
Lindsay <lindsay.t.smith@protonmail.com>
2025-02-27 13:56:49 +1100

    msg

---
+++ stringClass.java
@@ -0,0 +1,7 @@
+public class stringClass {   
+public static void main(String[] args) {
+//Create an object of Scanner class to help get input
+            String sentence = "I am a student of RMIT!";
+            System.out.println(sentence);
+        }
+    }
