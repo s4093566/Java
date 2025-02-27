f565edb5928b505c16af6e88e7d451108c9b6d05
Lindsay <lindsay.t.smith@protonmail.com>
2025-02-27 14:05:22 +1100

    msg

--- scanner/name.java
+++ scanner/name.java
@@ -4,13 +4,14 @@ public static void main(String[] args) {
 //Create an object of Scanner class to help get input
 Scanner myInput = new Scanner(System.in);
         //declare 4 memory boxes (variables)
-        String firstName, lastName;
+        String firstName, lastName, fullName;
         //get input
         System.out.print("Enter your First name: ");
         firstName = myInput.nextLine();
         System.out.print("Enter your Last name: ");
         lastName = myInput.nextLine();
-        System.out.println("Hello, my name is " + firstName + " "+ lastName);
+        fullName = firstName + " "+ lastName;
+        System.out.println("Hello, my name is " + fullName );
 
         
     }
